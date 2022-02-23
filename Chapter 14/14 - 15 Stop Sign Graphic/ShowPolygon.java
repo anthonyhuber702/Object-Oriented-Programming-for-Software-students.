5aca57668eaf44c2d78f26950591da4e6d6052e7
anthonyhuber702 <97196008+anthonyhuber702@users.noreply.github.com>
2022-02-23 14:43:49 -0700

    Add files via upload
    added stop sign

---
+++ Chapter 14/14 - 15 Stop Sign Graphic/ShowPolygon.java
@@ -0,0 +1,30 @@
+package graphics;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.*;
+
+public class ShowPolygon extends Application {
+	@Override // Override the start method in the Application class
+	public void start(Stage primaryStage) {   
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(new MyPolygon(), 400, 400);
+		primaryStage.setTitle("ShowPolygon"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+	}
+	
+	/**
+	 * The main method is only needed for the IDE with limited
+	 * JavaFX support. Not needed for running from the command line.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
