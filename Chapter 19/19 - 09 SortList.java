11a1de7b799a73b1da4b2df0b4bb63d034ad765e
anthonyhuber702 <97196008+anthonyhuber702@users.noreply.github.com>
2022-03-08 14:24:13 -0700

    Add files via upload

---
+++ Chapter 19/19 - 09 SortList.java
@@ -0,0 +1,37 @@
+package generics;
+
+import java.util.ArrayList;
+
+public class SortList{
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(14);
+    list.add(24);
+    list.add(4);
+    list.add(42);
+    list.add(5);
+    SortList.<Integer>sort(list);
+    
+    System.out.print(list);
+  }
+  public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+	  E currentMin;
+	  int currentMinIndex;
+	  
+	  for(int i = 0; i < list.size() - 1; i++) {
+		  currentMin = list.get(i);
+		  currentMinIndex = i;
+	  
+		  for(int j = i + 1; j < list.size(); j++) {
+			  if(currentMin.compareTo(list.get(j)) > 0) {
+				  currentMin = list.get(j);
+				  currentMinIndex = j;
+			  }
+		  }
+		  if(currentMinIndex != i) {
+			  list.set(currentMinIndex, list.get(i));
+			  list.set(i, currentMin);
+		  }
+	  }
+  }
+}
