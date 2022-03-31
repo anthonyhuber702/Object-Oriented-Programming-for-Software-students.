620bac330c32cc7a4028ce70725ba32dcc699b71
anthonyhuber702 <97196008+anthonyhuber702@users.noreply.github.com>
2022-03-31 13:16:49 -0600

    Add files via upload

---
+++ Chapter 24/24 - 05 Queue/GenericQueue.java
@@ -0,0 +1,18 @@
+package dsandalgs;
+import java.util.LinkedList;
+public class GenericQueue<E> extends LinkedList<E> {
+	private java.util.LinkedList<E> list = new java.util.LinkedList<>();
+	public void enqueue(E e) {
+		list.addLast(e);
+	}
+	public E dequeue () {
+		return list.removeFirst();
+	}
+	public int getSize() {
+		return list.size();
+	}
+	@Override
+	public String toString() {
+		return "Queue: " + list.toString();
+	}
+}
